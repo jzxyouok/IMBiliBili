7c47a12fde09131d45d5f8bb6d2c17859f13c608
tangzz6 <tangzz6@centaline.com.cn>
2016-06-04 10:56:28 +0800

    Initial Commit

---
+++ IMBiliBili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMBiliBili
+//
+//  Created by 汤振治 on 16/6/4.
+//  Copyright © 2016年 Imitations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMBiliBili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMBiliBili
+//
+//  Created by 汤振治 on 16/6/4.
+//  Copyright © 2016年 Imitations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
